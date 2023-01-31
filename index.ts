6237f9cd24657da534cb63a0e9ccd299527ef9a3
John Todd <jtodd2854@gmail.com>
2023-01-31 15:41:43 -0500

    write a console app that can parse and return the top n words in a file

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!')
+import * as fs from 'fs'
+import { getTopNWords } from './ParseText'
+
+if (process.argv.length < 3) {
+    console.log('Usage: node ' + process.argv[1] + ' FILENAME')
+    process.exit(1)
+}
+
+let filename: string = process.argv[2]
+fs.readFile(filename, 'utf-8', (err: NodeJS.ErrnoException, data: string) => {
+    if (err) throw err
+    console.log(getTopNWords(data))
+})
