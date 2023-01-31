6819253163f1ccb9669e15216fed954442ad7ded
John Todd <jtodd2854@gmail.com>
2023-01-31 14:57:11 -0500

    excludes common words

--- ParseText.ts
+++ ParseText.ts
@@ -1,6 +1,15 @@
-function getTopNWords(text: string, count: number = 50): string[] {
+let excludedWords: string[] = [
+    'the','of', 'to', 'and', 'a', 'in', 'is', 'it', 'you', 'that', 'he', 'was',
+    'for', 'on', 'are', 'with', 'as', 'i', 'his', 'they', 'be', 'at', 'one', 'have',
+    'this', 'from', 'or', 'had', 'by', 'not', 'word', 'but', 'what', 'some', 'we',
+    'can', 'out', 'other', 'were', 'all', 'there', 'when', 'up', 'use', 'your',
+    'how', 'said', 'an', 'each', 'she'
+]
+
+function getTopNWords(text: string, count: number = 50, exclude: string[] = excludedWords): string[] {
     let words = text.split(/\s+/)
         .map(word => word.toLowerCase())
+        .filter(word => !exclude.includes(word))
         .reduce((acc, el) => {
             if (acc[el]) {
                 acc[el]++
