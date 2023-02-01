d709a0522d06283482a56f25ff6aa41bbda846c9
John Todd <jtodd2854@gmail.com>
2023-02-01 09:30:24 -0500

    refactor to an aws lambda function

--- index.ts
+++ index.ts
@@ -1,22 +1,43 @@
 import * as https from 'https'
 import { getTopNWords } from './ParseText'
 
-if (process.argv.length < 3) {
-    console.log('Usage: node ' + process.argv[1] + ' FILENAME')
-    process.exit(1)
+exports.handler = async (event) => {
+    const url: string = event.url
+    console.log(`retrieving text from [${url}]`)
+    const promise = new Promise((resolve, reject) => {
+        if (!url) {
+            const response = {
+                statusCode: 400,
+                body: 'url not present in request'
+            }
+            reject(response)
+        } else {
+            https.get(url, resp => {
+                let data = ''
+
+                resp.on('data', chunk => {
+                    console.log(`retrieved chunk of size [${chunk.length}]`)
+                    data += chunk
+                })
+
+                resp.on('end', () => {
+                    console.log(`finished retrieving data`)
+                    const top50Words = getTopNWords(data)
+                    console.log(`finished counting words`)
+                    const response = {
+                        statusCode: 200,
+                        body: JSON.stringify(top50Words)
+                    }
+                    resolve(response)
+                })
+            }).on("error", err => {
+                const response = {
+                    statusCode: 400,
+                    body: `problem processing url [${url}]`
+                }
+                reject(response)
+            })
+        }
+    })
+    return promise
 }
-
-let url: string = process.argv[2]
-https.get(url, resp => {
-    let data = ''
-
-    resp.on('data', chunk => {
-        data += chunk
-    })
-
-    resp.on('end', () => {
-        console.log(getTopNWords(data))
-    })
-}).on("error", err => {
-    console.log("Error: " + err.message)
-})
